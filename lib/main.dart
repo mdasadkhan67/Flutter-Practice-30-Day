6f2da65ca8a90d296b1219f39b4251064a4bc610
ASAD KHAN <mdasadkhan67@gmail.com>
2022-12-21 12:08:48 +0530

    Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:thirtyday/home_page.dart';
+import 'package:thirtyday/screens/login_page.dart';
 
 void main() {
   runApp(ThirtyDayApp());
@@ -9,7 +10,12 @@ class ThirtyDayApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "login": (context) => LoginPage(),
+        "home": (context) => HomePage(),
+      },
     );
   }
 }
